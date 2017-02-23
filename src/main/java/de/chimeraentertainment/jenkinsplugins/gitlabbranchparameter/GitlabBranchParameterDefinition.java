c4ca6d7ec55dde4e169ee74bf8ba55e5159c1299
Max Walther <mwalther@chimera-entertainment.de>
2017-02-23 17:44:31 +0100

    - Added GitLab-Plugin dependency - Reworked the renamed Stash-Branch-Parameter-Plugin to work with GitLab

--- src/main/java/de/chimeraentertainment/jenkinsplugins/gitlabbranchparameter/GitlabBranchParameterDefinition.java
+++ src/main/java/de/chimeraentertainment/jenkinsplugins/gitlabbranchparameter/GitlabBranchParameterDefinition.java
@@ -1,31 +1,65 @@
 package de.chimeraentertainment.jenkinsplugins.gitlabbranchparameter;
 
+import com.dabsquared.gitlabjenkins.connection.GitLabConnectionProperty;
+import com.dabsquared.gitlabjenkins.service.GitLabProjectBranchesService;
 import hudson.Extension;
-import hudson.model.ParameterDefinition;
+import hudson.model.Job;
 import hudson.model.ParameterValue;
+import hudson.model.SimpleParameterDefinition;
 import hudson.model.StringParameterValue;
 import net.sf.json.JSONObject;
 import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.Ancestor;
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.Stapler;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
 
-public class GitlabBranchParameterDefinition extends ParameterDefinition
+public class GitlabBranchParameterDefinition extends SimpleParameterDefinition
 {
+	private static final Logger LOGGER = Logger.getLogger(GitlabBranchParameterDefinition.class.getName());
+
+	@Extension
+	public static class DescriptorImpl extends ParameterDescriptor {
+
+		public DescriptorImpl() {
+			load();
+		}
+
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+			save();
+			return super.configure(req, json);
+		}
+
+		@Override
+		public GitlabBranchParameterDefinition newInstance(StaplerRequest req, JSONObject formData) throws FormException
+		{
+			return new GitlabBranchParameterDefinition(
+				formData.getString("name"),
+				formData.getString("description"),
+				formData.getString("repository")
+			);
+		}
+
+		@Override
+		public String getDisplayName() {
+			return "GitLab Branch Parameter";
+		}
+	}
+
 	private String repository;
-	private String defaultValue;
-	private String branchNameRegex;
-	private String tagNameRegex;
 
 	@DataBoundConstructor
-	public GitlabBranchParameterDefinition(String name, String description, String repository, String defaultValue)
+	public GitlabBranchParameterDefinition(String name, String description, String repository)
 	{
-		super(name, description);
+		super(StringUtils.trim(name), description);
 		this.repository = repository;
-		this.defaultValue = defaultValue;
 	}
 
 	public String getRepository()
@@ -33,91 +67,48 @@ public class GitlabBranchParameterDefinition extends ParameterDefinition
 		return repository;
 	}
 
-	public void setRepository(String repository)
+	public void setRepository(String value)
 	{
-		this.repository = repository;
-	}
-
-	public String getDefaultValue()
-	{
-		return defaultValue;
-	}
-
-	public void setDefaultValue(String defaultValue)
-	{
-		this.defaultValue = defaultValue;
-	}
-
-	public String getBranchNameRegex() {
-		return branchNameRegex;
-	}
-
-	@DataBoundSetter
-	public void setBranchNameRegex(String branchNameRegex) {
-		this.branchNameRegex = branchNameRegex;
-	}
-
-	public String getTagNameRegex() {
-		return tagNameRegex;
-	}
-
-	@DataBoundSetter
-	public void setTagNameRegex(String tagNameRegex) {
-		this.tagNameRegex = tagNameRegex;
+		this.repository = value;
 	}
 
 	@Override
-	public ParameterValue createValue(StaplerRequest staplerRequest, JSONObject jsonObject)
+	public ParameterValue createValue(StaplerRequest request, JSONObject jo)
 	{
-		String value = jsonObject.getString("value");
-		return new StringParameterValue(this.getName(), value);
+		StringParameterValue value = request.bindJSON(StringParameterValue.class, jo);
+		value.setDescription(getDescription());
+		return value;
 	}
 
 	@Override
-	public ParameterValue createValue(StaplerRequest staplerRequest)
+	public ParameterValue createValue(String value) throws IllegalArgumentException
 	{
-		String[] parameterValues = staplerRequest.getParameterValues(getName());
-		String value = parameterValues[0];
-		return new StringParameterValue(this.getName(), value);
+		return new StringParameterValue(getName(), value, getDescription());
 	}
 
-	@Override
-	public ParameterValue getDefaultParameterValue()
+	public Map<String, String> getBranches() throws IOException
 	{
-		return new StringParameterValue(this.getName(), defaultValue);
+		return computeBranches();
 	}
 
-	public Map<String, Map<String, String>> getDefaultValueMap() throws IOException
-	{
-		return computeDefaultValueMap();
-	}
-
-	private Map<String, Map<String, String>> computeDefaultValueMap() throws IOException
-	{
-		String project = repository.split("/")[0];
-		String repo = repository.split("/")[1];
-		GitlabConnector connector = new GitlabConnector(getDescriptor().getApiUrl(), getDescriptor().getUsername(), getDescriptor().getPassword());
-
-		Map<String, String> map = connector.getBranches(project, repo, branchNameRegex);
-		if (StringUtils.isNotBlank(defaultValue))
-		{
-			map.put(defaultValue, defaultValue);
+	private Job<?, ?> retrieveCurrentJob() {
+		StaplerRequest request = Stapler.getCurrentRequest();
+		if (request != null) {
+			Ancestor ancestor = request.findAncestor(Job.class);
+			return ancestor == null ? null : (Job<?, ?>) ancestor.getObject();
 		}
-		map.putAll(connector.getTags(project, repo, tagNameRegex));
-
-		return MapsUtils.groupMap(map);
+		return null;
 	}
 
-	@Override
-	public GitlabBranchParameterDescriptorImpl getDescriptor()
+	private Map<String, String> computeBranches() throws IOException
 	{
-		return (GitlabBranchParameterDescriptorImpl) super.getDescriptor();
+		Job<?, ?> project = retrieveCurrentJob();
+		GitLabConnectionProperty connectionProperty = project.getProperty(GitLabConnectionProperty.class);
+		List<String> branches = GitLabProjectBranchesService.instance().getBranches(connectionProperty.getClient(), repository);
+		Map<String, String> map = new TreeMap<>();
+		for (String branch: branches) {
+			map.put(branch, branch);
+		}
+		return map;
 	}
-
-	@Extension
-	public static class GitlabBranchParameterDescriptorImpl extends GitlabBranchParameterDescriptor
-	{
-
-	}
-
 }
